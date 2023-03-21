68c383253b560eae4c666da9b2ec5feb081d4a9b
Dennis <10233439+idea404@users.noreply.github.com>
2023-03-21 12:49:32 +0100

    stringify array objects

--- examples/likes_timestamped/indexingLogic.js
+++ examples/likes_timestamped/indexingLogic.js
@@ -5,39 +5,37 @@ async function getBlock(block, context) {
     return JSON.parse(buff.toString("utf-8"));
   }
 
-  const nearSocialLikes = block
+  const nearSocialPosts = block
     .actions()
     .filter((action) => action.receiverId === SOCIAL_DB)
     .flatMap((action) =>
       action.operations
         .map((operation) => operation["FunctionCall"])
         .filter((operation) => operation?.methodName === "set")
-        .filter((functionCall) => {
-          const accountId = Object.keys(functionCall.args.data)[0];
-          const hasLike = functionCall.args.data[accountId].index?.like;
-          hasLike == true;
-        })
         .map((functionCallOperation) => ({
           ...functionCallOperation,
           args: base64decode(functionCallOperation.args),
           receiptId: action.receiptId,
         }))
     );
-  if (nearSocialLikes.length > 0) {
+
+  if (nearSocialPosts.length > 0) {
     const blockHeight = block.blockHeight;
     const blockTimestamp = block.header().timestampNanosec;
     const formatted_likes = [];
-    nearSocialLikes.forEach((likeAction) => {
-      const accountId = Object.keys(likeAction.args.data)[0];
-      const likeData = {
-        account_id: accountId,
-        block_height: blockHeight,
-        block_timestamp: blockTimestamp,
-        receipt_id: likeAction.receiptId,
-        like: likeAction.args.data[accountId].index.like,
-        post_id: likeAction.args.data[accountId].index.like.key.path + blockHeight,
-      };
-      formatted_likes.push(likeData);
+    nearSocialPosts.forEach((postAction) => {
+      const accountId = Object.keys(postAction.args.data)[0];
+      if (postAction.args.data[accountId].index.like) {
+        const likeData = {
+          account_id: accountId,
+          block_height: blockHeight,
+          block_timestamp: blockTimestamp,
+          receipt_id: postAction.receiptId,
+          like: postAction.args.data[accountId].index.like,
+          post_id: postAction.args.data[accountId].index.like.key.path + blockHeight,
+        };
+        formatted_likes.push(JSON.stringify(likeData));
+      }
     });
     await context.set(blockHeight.toString(), JSON.stringify(formatted_likes));
   }
